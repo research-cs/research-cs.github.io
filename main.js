61a03c5c816206589dd217f4a127071084c5cb18
Helena Vasconcelos <helenavascc@gmail.com>
2022-07-02 13:59:28 -0700

    no modal, just on previous page

--- main.js
+++ main.js
@@ -1352,11 +1352,11 @@ function renderTask(condition, data, callback=null) {
     $('#text-top-task').html(bonus_text)
     $('#text-top-task-col').show()
 
-    if ((curr_correct + curr_incorrect) == 0) {
-      var collaboration_modal_text_for_bonus = "For the first half of this study, you will receive " + curr_bonus_collaboration + " cent(s) in bonus. For the second half of this study, you will receive " +inteleaved_blocked_benefit[1] + " cent(s) in bonus."
-      $('#bonus-modal-text').html(collaboration_modal_text_for_bonus)
-      $('#bonus-interleaved-modal').modal('toggle')
-    }
+    // if ((curr_correct + curr_incorrect) == 0) {
+    //   var collaboration_modal_text_for_bonus = "For the first half of this study, you will receive " + curr_bonus_collaboration + " cent(s) in bonus. For the second half of this study, you will receive " +inteleaved_blocked_benefit[1] + " cent(s) in bonus."
+    //   $('#bonus-modal-text').html(collaboration_modal_text_for_bonus)
+    //   $('#bonus-interleaved-modal').modal('toggle')
+    // }
 
   }
   if (current_condition == 'prediction' || training_phase) {
@@ -1769,7 +1769,8 @@ function repeatTask(condition, id ='switch-tasks') {
         // $(length_id).html('short')
           // $('#coin-explanation').html('Note that 100 silver credits equals $0.05.')
      }
-
+     var collaboration_modal_text_for_bonus = "For the first half of this study, you will receive " + curr_bonus_collaboration + " cent(s) in bonus. For the second half of this study, you will receive " +inteleaved_blocked_benefit[1] + " cent(s) in bonus."
+      $(coin_id).html(collaboration_modal_text_for_bonus)
       
      if (current_xai_setting == 'xai' || ai_condition[1] == 'xai-arrow'|| ai_condition[1] == 'xai-written' ||
       ai_condition[1] == 'xai-white-salient' || ai_condition[1] == 'xai-blue-salient') {
