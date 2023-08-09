9c16c8be88589491802152210ce09902cc14e502
research-cs <85696481+research-cs@users.noreply.github.com>
2023-08-09 14:52:36 -0700

    Create variable.js

---
+++ templates/main-study-js/variable.js
@@ -0,0 +1,27 @@
+var questions_column_width;
+
+var startTime;
+
+let screen_order = generate_study_array(["feed-selection", "post-placement", "feed-comparison-B"], [1, 2, 5], 2, "left-feed-questions");
+
+var post_index = 0;
+var screen_index = 0;
+
+var feature_dict = 
+{
+    "days_since_posted": "Hours Since Posted", 
+    "like_count": "Like Count", 
+    "view_count": "View Count", 
+    "comment_count": "Comment Count", 
+    "character_count": "Character Count"
+};
+
+var feature_keys = Object.keys(feature_dict);
+var feature_names = Object.values(feature_dict);
+
+
+var start_times = {};
+
+output = {
+    
+}
